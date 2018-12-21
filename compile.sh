5ad9a2c1d4576ac948a1cd3f1b2edbbc74acdfc2
Hans Ren <171357351@qq.com>
2018-12-21 16:10:32 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/en sh
-g++ -I/home/hans/ncnn-20180516/src -I/home/hans/ncnn-20180516/build/src -include"./PCN.h" -O0 -g3 -Wall -c -fmessage-length=0  -std=c++11 -MMD -MP -MF"cam.d" -MT"cam.o" -o "cam.o" "./cam.cpp"
-g++ -fopenmp -o "ncnn_PCN_cam"  ./cam.o  /home/hans/ncnn-20180516/build/src/libncnn.a -lopencv_contrib -lopencv_core -lopencv_highgui -lopencv_imgproc
+g++ -I/home/hans/ncnn-20180516/src -I/home/hans/ncnn-20180516/build/src -include"./PCN.h" -O3 -c -std=c++11 -fPIC -o "cam.o" "./cam.cpp"
+g++ -fopenmp -o "ncnn_PCN_cam"  ./cam.o  /home/hans/ncnn-20180516/build/src/libncnn.a -lopencv_core -lopencv_highgui -lopencv_imgproc
